8a0690fcf70444cfb27a84a3908c8347029d3d24
chasdevs <chasdeveas@gmail.com>
2019-04-13 17:11:30 -0400

    Working S3 integration test.

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,2 +1,12 @@
+import os
+import boto3
+
+s3 = boto3.resource('s3')
+fixtures_dir = os.path.join(os.path.dirname(__file__), 'fixtures')  # <-- absolute dir the script is in
+
 def test_put_file():
-    assert False
+    scrape_fixture = os.path.join(fixtures_dir, 'dummy_scrape_data.json')
+    json_file = open(scrape_fixture, 'rb')
+    result = s3.Object('project-curate-test', 'dummy_scrape_data.json').put(Body=json_file)
+    print(result)
+    assert result['ResponseMetadata']['HTTPStatusCode'] == 200
