b398d6776ecbf42abba0ad54c7f587d1da5f6f2d
Chas DeVeas <chasdeveas@gmail.com>
2019-10-16 15:27:26 -0400

    Basic test writing a JSON fixture to S3 test bucket. (#2)

---
+++ tests/test_s3.py
@@ -0,0 +1,11 @@
+import os
+import boto3
+
+s3 = boto3.resource('s3')
+fixtures_dir = os.path.join(os.path.dirname(__file__), 'fixtures')  # <-- absolute dir the script is in
+
+def test_put_file():
+    scrape_fixture = os.path.join(fixtures_dir, 'dummy_scrape_data.json')
+    json_file = open(scrape_fixture, 'rb')
+    result = s3.Object('project-curate-test', 'dummy_scrape_data.json').put(Body=json_file)
+    assert result['ResponseMetadata']['HTTPStatusCode'] == 200
